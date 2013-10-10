01eaa79ae25dc140f2e5edbb549b923a4422e056
Fabien Boucher <fabien.boucher@enovance.com>
2013-10-10 14:33:39 +0200

    Support Swift Object Store as Git bare repository
    This commit provides the minimal requirements to
    use OpenStack Swift as backend for Dulwich.
    The Swift Git repository only use pack and index
    files to store objects and references are managed
    through the info/refs file.

--- dulwich/tests/__init__.py
+++ dulwich/tests/__init__.py
@@ -133,6 +133,7 @@ def self_test_suite():
         'server',
         'walk',
         'web',
+        'swift',
         ]
     module_names = ['dulwich.tests.test_' + name for name in names]
     loader = unittest.TestLoader()
